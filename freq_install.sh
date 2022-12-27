4f33d80d20d150902ad8fe3ba8ec64934775b83d
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-12-27 17:23:52 -0600

    Update freq_install.sh

--- freq_install.sh
+++ freq_install.sh
@@ -36,19 +36,19 @@ git checkout stable
 # --install, Install freqtrade from scratch
 ./setup.sh -i
 
-function code() {
-    bash -c 'curl -fsSL https://code-server.dev/install.sh | sh'
-    bash -c 'sudo systemctl enable --now code-server@$USER'
-}
+# function code() {
+#     bash -c 'curl -fsSL https://code-server.dev/install.sh | sh'
+#     bash -c 'sudo systemctl enable --now code-server@$USER'
+# }
 
-echo 'Would you like to install code-server? Enter: y/n'
-read codeCond
+# echo 'Would you like to install code-server? Enter: y/n'
+# read codeCond
 
-if [ $codeCond == 'y' ]
-then
-  echo "installing code-server"
-  code
-fi
+# if [ $codeCond == 'y' ]
+# then
+#   echo "installing code-server"
+#   code
+# fi
 
 echo '# activate env'
 echo 'source ./.env/bin/activate'
