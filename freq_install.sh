08f84034be26ee909ad2be51c6cf26f24f5fcdea
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-12-27 10:26:40 -0600

    Update freq_install.sh

--- freq_install.sh
+++ freq_install.sh
@@ -6,7 +6,7 @@ tar xvzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib
 sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h
 ./configure --prefix=/usr/local
-make
+make -j
 sudo make install
 # On debian based systems (debian, ubuntu, ...) - updating ldconfig might be necessary.
 sudo ldconfig  
